376480820c81b1d9eb96fece466a600ab1910f78
Craig Wann <craigwann1@gmail.com>
2015-01-17 20:10:52 -0600

    Adding environment config.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead'),
+	'local' => array('Craigs-iMac.ok.cox.net'),
+    'staging' => array('ip-10-252-79-136')
 
 ));
 
